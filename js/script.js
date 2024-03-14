312119a6e54cba4e95f62bdcb537f6b1bbea841e
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-03-14 12:36:48 +0500

    homework

---
+++ js/script.js
@@ -0,0 +1,111 @@
+
+
+// let listItem = document.getElementsByTagName('li');
+// console.log(listItem[1]);
+
+
+// let listItem = document.getElementsByClassName('item');
+// console.log(listItem[0]);
+
+// let clicbtn = document.getElementById('clicbtn');
+// console.log(clicbtn);
+
+
+// let listItem = document.querySelector('li');
+// let listItem = document.querySelector('.item');
+// let listItem = document.querySelector('#list');
+
+// listItem.forEach((item) => {
+//     console.log(item)
+// });
+
+
+
+// console.log(listItem);
+
+
+// let listItem = document.querySelectorAll('#list');
+// console.log(listItem);
+
+
+// let listItem = document.getElementById('item');
+// listItem.textContent += ' Samandar'
+
+// let listItem = document.querySelectorAll('.item')
+
+// listItem.forEach((res) => {
+// res.textContent += ' dev'
+// })
+// console.log(listItem);
+
+
+// let listItem = document.querySelectorAll('#item');
+
+// listItem.forEach((res) => {
+// res.innerHTML = '<p> SAMANDAR</p>'
+// })
+
+// console.log(listItem);
+
+
+// let res = ['samandar', 'safarali', 'sarvar', 'doniyor'];
+
+// let ol = document.querySelector('ol');
+// res.forEach((res) => {
+// ol.innerHTML += `<li>${res}</li>`
+// })
+
+// console.log(res);
+
+// let res = document.querySelector('a');
+// res.setAttribute('href','https://www.youtube.com/watch?v=D3C8zir3ad8&list=PLNS3PujVHR-ay0HpqbsPWccgEhJZk6K9u&index=50&ab_channel=AkhrorWeb')
+// res.textContent = 'Akhror Web Youtube kanali'
+// console.log(res.getAttribute('href'));
+
+
+// const text = document.querySelector('p')
+// // console.log(text.getAttribute('class'))
+// text.setAttribute('class', 'samandar')
+// console.log(text.getAttribute('class'));
+
+
+// let listItem = document.querySelector('h1');
+// // listItem.setAttribute('style', 'margin:50px')
+// console.log(listItem.style);
+// listItem.style.margin = '50px';
+// listItem.style.color = 'crimson';
+
+// let listItem = document.querySelector('p');
+// console.log(listItem.classList);
+// listItem.classList.add('sucses');
+// listItem.classList.remove('sucses')
+
+// let listItem = document.querySelectorAll('h4');
+
+// listItem.forEach((text) => {
+// if (text.textContent.includes('success')) {
+//     text.classList.add('success')
+// }
+
+// if (text.textContent.includes('erorr')) {
+//     text.classList.add('erorr')
+// }
+// })
+// console.log(listItem);
+
+// let article = document.querySelector('article');
+// console.log(article.children);
+
+// Array.from(article.children).forEach((child) => {
+// child.classList.add('article-childrn')
+// })
+
+// let ul = document.querySelector('ul')
+
+
+// let button = document.querySelector('button')
+
+// button.addEventListener('sarvar', () => {
+//     ul.innerHTML += '<li>safarali</li>'
+// });
+
